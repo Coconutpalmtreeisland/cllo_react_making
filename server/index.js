29d03b9c5042063cd1b334a4279c15690b4f08b7
coconutpalmtreeisland <adorableness1215@naver.com>
2023-12-27 15:02:21 +0900

    23.12.27 서버 작업 중

--- server/index.js
+++ server/index.js
@@ -8,14 +8,13 @@ const port = 5050;
 // const port = process.env.PORT || 5050;
 // const config = require("./server/config/key.js");
 
-app.use(express.static(path.join(__dirname, "./client/build")));
-app.use("/image", express.static("./image"));
+app.use(express.static(path.join(__dirname, "../client/build")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // express router
 // app.use("/api/post", require("./server/router/post.js"));
-app.use("/api/user", require("./server/router/user.js"));
+app.use("/api/user", require("../server/router/user.js"));
 // app.use("/api/reple", require("./server/router/reple.js"));
 
 app.listen(port, () => {
